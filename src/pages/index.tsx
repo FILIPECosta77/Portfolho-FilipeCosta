c742c2061d4039ab4070bdefc00d38f30a512d3d
Filipe Costa <84087378+FILIPECosta77@users.noreply.github.com>
2023-08-09 14:38:40 -0300

    refactor:switch MUI for Chakra-UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,18 @@
-import { Profission } from "@/components/cargos";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
+import { Box } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 import Head from "next/head";
-import { Description } from "@/components/description";
 
-export default function Home() {
+const Home = () => {
   return (
-    <>
+    <Box w={"100vw"} h={"100vh"} backgroundColor={"brand.1"}>
       <Head>
         <title>Filipe Costa</title>
       </Head>
-
-      <Box
-        component="header"
-        sx={{
-          width: "100%",
-          height: 400,
-          backgroundColor: "primary.main",
-          display: "flex",
-        }}
-      >
-        <Stack
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          spacing={6}
-        >
-          <Profission number={1} />
-          <Description />
-        </Stack>
-      </Box>
-    </>
+      <Text fontSize={"6xl"} fontWeight={700} color={"brand.2"}>
+        Desenvolvedor FullStack
+      </Text>
+    </Box>
   );
-}
+};
+
+export default Home;
--- src/styles/themes/globalTheme.ts
+++ src/styles/themes/globalTheme.ts
@@ -1,28 +1,12 @@
-import { ThemeOptions } from "@mui/material/styles";
+import { extendTheme } from "@chakra-ui/react";
 
-export const themeOptions: ThemeOptions = {
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#5AA9E6",
-      contrastText: "#FFE45E",
-    },
-    error: {
-      main: "#d42f2f",
-      light: "#dc5858",
-      dark: "#941f1f",
-      contrastText: "#ffffff",
-    },
-    success: {
-      main: "#2e7d32",
-      light: "#599a5d",
-      dark: "#205823",
-      contrastText: "#ffffff",
-    },
-    text: {
-      primary: "#000",
-      secondary: "#fff",
-      disabled: "#555",
+const theme = extendTheme({
+  colors: {
+    brand: {
+      1: "#5AA9E6",
+      2: "#FFE45E",
     },
   },
-};
+});
+
+export default theme;
